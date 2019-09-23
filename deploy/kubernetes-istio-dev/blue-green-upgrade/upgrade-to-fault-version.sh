60432b8469fb42dd48d49f4b7cf318d6559fedc2
RU XING XIAO <xiaorux@cn.ibm.com>
2019-09-23 17:24:43 +0800

    fix shell error

--- deploy/kubernetes-istio-dev/blue-green-upgrade/upgrade-to-fault-version.sh
+++ deploy/kubernetes-istio-dev/blue-green-upgrade/upgrade-to-fault-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$(dirname "$0")
-ROOT_DIR=$(cd $SCRIPT_DIR/..; pwd)
+ROOT_DIR=$(cd "$SCRIPT_DIR"/.. || exit; pwd)
 
 if [[ -z "$PRE_VERSION" ]] ; then
     echo "Cannot find PRE_VERSION var"
@@ -12,38 +12,38 @@ if [[ -z "$NEW_VERSION" ]] ; then
     exit 1
 fi
 
-$pv=$PRE_VERSION
-$nv=$NEW_VERSION
+pv=$PRE_VERSION
+nv=$NEW_VERSION
 
 echo "Starting blue green upgrading front-end service from $pv to version $nv"
-kubectl delete -f $ROOT_DIR/manifests-versions/front-end-dep-$nv.yaml
+kubectl delete -f "$ROOT_DIR/manifests-versions/front-end-dep-$nv.yaml"
 echo "Apply deployment version $nv to kube"
-kubectl apply -f $ROOT_DIR/manifests-versions/front-end-dep-$nv.yaml
+kubectl apply -f "$ROOT_DIR/manifests-versions/front-end-dep-$nv.yaml"
 
 echo "Add route traffic to $nv based on http header x-version=$nv, leaving all other to $pv"
-kubectl apply -f $ROOT_DIR/manifest-networking/svc-fault-$nv-added.yaml
+kubectl apply -f "$ROOT_DIR/manifest-networking/svc-fault-$nv-added.yaml"
 
 echo "Start canary testing"
-cd /opt/js-engine-dev
-node index.js -f sock-shop-header.js -c username=test -c password=test -a $pv -a $nv
+cd /opt/js-engine-dev || exit
+node index.js -f sock-shop-header.js -c "username=test" -c "password=test" -a "$pv" -a "$nv"
 level=$?
 
-if [ $level == 2]
+if [ $level == 2 ]
 then 
     msg="version $nv synthetic test failure, remove route traffic to the version $nv"
-    rollback msg $pv $nv
-elif [ $level == 1]
-    msg="version $nv respsoneTime is slower than previous version $pv, remove route traffic to the version $nv"
-    rollback msg $pv $nv
-else
+    rollback msg "$pv" "$nv"
+elif [ $level == 1 ]
 then
-   cd $SCRIPT_DIR
+msg="version $nv respsoneTime is slower than previous version $pv, remove route traffic to the version $nv"
+rollback msg "$pv" "$nv"
+else
+   cd "$SCRIPT_DIR" || exit
    array=( 10 50 100 )
    for i in "${array[@]}"
    do
-      weighttest $i $pv $nv
+      weighttest "$i" "$pv" "$nv"
       level=$?
-      if [ $level !=0 ]
+      if [ $level != 0 ]
       then
          break
       fi
@@ -54,11 +54,11 @@ rollback(){
     local msg=$1
     local pv=$2
     local nv=$3   
-    echo $msg
-    cd $SCRIPT_DIR
-    kubectl apply -f $ROOT_DIR/manifest-networking/svc-normal-$pv.yaml
+    echo "$msg"
+    cd "$SCRIPT_DIR" || exit
+    kubectl apply -f "$ROOT_DIR/manifest-networking/svc-normal-$pv.yaml"
     echo "String remove deployment of version $nv"
-    kubectl delete -f $ROOT_DIR/manifests-versions/front-end-dep-$nv.yaml
+    kubectl delete -f "$ROOT_DIR/manifests-versions/front-end-dep-$nv.yaml"
     echo "Remove version $nv finished"
 }
 
@@ -67,21 +67,23 @@ weighttest(){
    local pv=$2
    local nv=$3
    echo "Add route traffic to $nv weighted $rw, leaving all other to $pv"
-   kubectl apply -f $ROOT_DIR/manifest-networking/svc-normal-$nv-$rw.yaml
+   kubectl apply -f "$ROOT_DIR/manifest-networking/svc-normal-$nv-$rw.yaml"
    echo "Start canary testing"
-   cd /opt/js-engine-dev
-   node index.js -f sock-shop-header.js -c username=test -c password=test -a $pv -a $nv
+   cd /opt/js-engine-dev || exit
+   node index.js -f sock-shop-header.js -c username=test -c password=test -a "$pv" -a "$nv"
    level=$?
-   if [ $level == 2]
+   
+   if [ $level == 2 ]
    then
      msg="version $nv with route weight $rw synthetic test failure, remove route traffic to the version $nv"
-     rollback msg $pv $nv
+     rollback msg "$pv" "$nv"
      return 2 
-   elif [ $level == 1]
-     if [ $rw > 50 ]
+   elif [ $level == 1 ]
+   then
+     if [ "$rw" -gt 50 ]
      then
        msg="version $nv with route weight $rw respsoneTime is slower than previous version $pv, remove route traffic to the version $nv"
-       rollback msg $pv $nv
+       rollback msg "$pv" "$nv"
        return 1 
      else
        return 0
